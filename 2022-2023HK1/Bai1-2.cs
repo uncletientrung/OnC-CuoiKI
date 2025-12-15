2383594408a4a908c212cc9ee3ef4724afbd73e2
uncletientrung <thihachcf@gmail.com>
2025-12-15 21:58:45 +0700

    phan 1 2022-2023 HK1

---
+++ 2022-2023HK1/Bai1-2.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System;
+
+class Program
+{
+    // CÂU 1 Viết phương thức xác định năm nhuận
+    public static bool IsLeapYear(int year)
+    {
+        return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
+    }
+    // CÂU 2 Viết phương thức đảo ngược chuỗi
+    public static void ReverseString(string a)
+    {
+        char []chars = a.ToCharArray();
+        string result = "";
+        for(int i = chars.Length - 1; i >= 0; i--)
+        {
+            result += chars[i];
+        }
+        Console.WriteLine(result);
+    }
+    // Câu 4 khởi tạo và sử dụng một delegate có kiểu trả về là int và tham số truyền vào là kiểu string.
+    // Delegate này sẽ được sử dụng cho mỗi phương thức
+    static int ConverStringToInt(string number)
+    {
+        return int.Parse(number);
+    }
+    static int ShowString(string s)
+    {
+        Console.WriteLine(s);
+        return s.Length;
+    }
+    delegate int MyDelegate(string value);
+    public static void Main(string[] args)
+    {
+        //Console.WriteLine(IsLeapYear(2024));
+        //ReverseString("Hello");
+
+        // Khởi tạo delegate với phương thức ConvertStringToInt
+        MyDelegate d1 = ConverStringToInt;
+        int result1 = d1("123");
+        Console.WriteLine("ConvertStringToInt result: " + result1);
+        // Gán delegate sang phương thức ShowString
+        MyDelegate d2 = ShowString;
+        int result2 = d2("Hello CSharp");
+        Console.WriteLine("ShowString result: " + result2);
+    }
+}
---
+++ 2022-2023HK1/Bai3.cs
@@ -0,0 +1,46 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace _2022_2023HK1
+//{
+//    internal class Bai3
+//    {
+//        private String Name;
+//        public Bai3(string name)
+//        {
+//            Name = name;
+//        }   
+//        private void ShowSerect()
+//        {
+//            Console.WriteLine("your name" + Name);
+//        }
+//        private static void DoSomeThing(string job)
+//        {
+//            Console.WriteLine("Do job" + job);
+//        }
+//        class Diary
+//        {
+//            public void Logging()
+//            {
+//                //ShowSerect(); // Static ảnh hưởng đến cách gọi không ảnh hưởng đến gọi tử đâu
+//                    // Lỗi gì ShowSerect là non-static
+//                DoSomeThing("Code Csharp"); 
+                
+//            }
+//        }
+//    }
+//    class Test
+//    {
+//        public static void Main(string[] args)
+//        {
+//            Bai3 a = new Bai3("A");
+//            //string name = a.Name; // Lỗi vì Name là private không có get set
+//            //a.ShowSerect(); // Lỗi private luôn
+//            //a.DoSomeThing(); // Lỗi private và thiếu tham số
+//        }
+//    }
+    
+//}
